a4abcb698e2abc25149b181f69b7bfccb0abbe2f
zoubjd <zouhairbajdouri19@gmail.com>
2024-05-19 21:13:53 +0100

    tests for basemodel

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,51 +1,45 @@
 #!/usr/bin/python3
-"""
-Test suite for base_model
-"""
+"""Test suite for base_model"""
 import unittest
-from datetime import datetime
 from models.base_model import BaseModel
+from datetime import datetime
+import uuid
 
 
 class TestBaseModel(unittest.TestCase):
     """Test cases for the base_model"""
-    def setUp(self):
-        """Set up for the tests"""
-        self.base = BaseModel()
-
     def test_str(self):
-        """Checks the string output"""
-        expected_str = f"[{type(self.base).__name__}] ({self.base.id}) {self.base.__dict__}"
-        self.assertEqual(str(self.base), expected_str)
+        """checks the string"""
+        base = BaseModel()
+        self.assertEqual(base.__str__(),
+                         f"[{type(base).__name__}] \
+({base.id}) {base.__dict__}")
 
     def test_to_dict(self):
-        """Checks to_dict()"""
-        base_dict = self.base.to_dict()
-        self.assertIsInstance(base_dict, dict)
-        self.assertEqual(base_dict['__class__'], 'BaseModel')
-        self.assertEqual(base_dict['id'], self.base.id)
-        self.assertEqual(base_dict['created_at'], self.base.created_at.isoformat())
-        self.assertEqual(base_dict['updated_at'], self.base.updated_at.isoformat())
-
-        prev_time = self.base.updated_at
-        self.base.save()
-        self.assertNotEqual(prev_time, self.base.updated_at)
+        """checks to_dict()"""
+        base = BaseModel()
+        prev_time = base.updated_at
+        self.assertDictEqual(base.to_dict(),
+                             {'__class__': type(base).__name__,
+                              'updated_at': base.updated_at.isoformat(),
+                              'id': base.id,
+                              'created_at': base.created_at.isoformat()})
+        base.save()
+        self.assertNotEqual(prev_time, base.updated_at)
 
     def test_attr_classes(self):
-        """Checks the attributess"""
+        """checks classes and attributes"""
+        base = BaseModel()
         base2 = BaseModel()
-        self.assertIsInstance(self.base.id, str)
-        self.assertIsInstance(self.base.created_at, datetime)
-        self.assertIsInstance(self.base.updated_at, datetime)
-        self.assertNotEqual(self.base.id, base2.id)
+        self.assertIsInstance(base.id, str)
+        self.assertIsInstance(base.created_at, datetime)
+        self.assertIsInstance(base.updated_at, datetime)
+        self.assertNotEqual(base.id, base2.id)
 
     def test_save(self):
-        """Tests save """
-        prev_time = self.base.updated_at
-        self.base.save()
-        new_time = self.base.updated_at
-        self.assertNotEqual(prev_time, new_time)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        """tests the save """
+        base = BaseModel()
+        prevtime = base.updated_at
+        base.save()
+        newtime = base.updated_at
+        self.assertNotEqual(prevtime, newtime)
